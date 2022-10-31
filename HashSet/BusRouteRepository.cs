53e76523eb94f274867da6bfb78c926fe2ff0a1b
Mukesh Gupta <Mukesh.Gupta@Klingelnberg.com>
2022-10-31 11:18:09 +0530

    HAshset

---
+++ HashSet/BusRouteRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace BusRouteApplication
+{
+    public class BusRouteRepository
+    {
+        private readonly BusRoute[] _allRoutes;
+
+        public BusRouteRepository()
+        {
+            _allRoutes = new BusRoute[] {
+                new BusRoute(40, new string[] { "Morcambe", "Lancaster", "Garstang", "Preston","Carnforth" }),
+                new BusRoute(42, new string[] { "Lancaster", "Garstang", "Blackpool" }),
+                new BusRoute(100, new string[] { "University", "Lancaster", "Morecambe" }),
+                new BusRoute(555, new string[] { "Lancaster", "Carnforth", "Kendel", "Windermere", "Keswik" }),
+                new BusRoute(5, new string[] { "Overton", "Morcambe", "Carnforth" })
+            };
+        }
+   
+
+
+        public BusRoute[] FindBusesTo(string location )
+        {
+            return Array.FindAll(_allRoutes, route => route.Serves(location));
+        }
+
+        public BusRoute[] FindBusesBetween(string location1,string location2)
+        {
+            return Array.FindAll(_allRoutes, route => (route.Serves(location1) &&  route.Serves(location2)));
+        }
+    }
+}
---
+++ HashSet/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;//collections are provided
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//need of List<> :array cannot remove the specific element at specific index
+namespace BusRouteApplication
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            BusRouteRepository repository = new BusRouteRepository();
+
+            Console.WriteLine("Where Are u?");
+            string startingAt = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Where do you want to go to?");
+            string goingTo = Console.ReadLine();
+            //BusRoute[] routes = repository.FindBusesBetween(startingAt, goingTo);
+
+            BusRoute[] source = repository.FindBusesTo(startingAt);
+            BusRoute[] destination=repository.FindBusesTo(goingTo);
+
+            HashSet<BusRoute> routes = new HashSet<BusRoute>(source);
+            routes.IntersectWith(destination);
+
+
+            if (routes.Count>0)
+                foreach (BusRoute route in routes)
+                    Console.WriteLine($"You can use route {route}");
+            else
+                Console.WriteLine($"No routes go from {startingAt} to {goingTo}'");
+
+        }         
+    }
+}
