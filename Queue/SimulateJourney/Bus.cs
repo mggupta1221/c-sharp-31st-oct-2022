5fd9de2e8d744e70317b89183ad5d94a148269c0
Mukesh Gupta <Mukesh.Gupta@Klingelnberg.com>
2022-10-31 15:49:36 +0530

    Queue

---
+++ Queue/SimulateJourney/Bus.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulateJourney
+{
+    public class Bus
+    {
+        public const int CAPACITY = 5;
+        public int Space { get { return CAPACITY - _passengers.Count; } }
+
+        private List <Passenger> _passengers=new List<Passenger>();
+
+        public bool Load(Passenger passenger)
+        {
+            if (Space < 1)
+                return false;
+
+           _passengers.Add(passenger);
+            Console.WriteLine($"{passenger} got on Bus");
+            return true;
+        }
+    }
+}
---
+++ Queue/SimulateJourney/BusStop.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace SimulateJourney
+{
+    public class BusStop
+    {
+        private Queue<Passenger> _personWaiting=new Queue<Passenger>();
+
+        public void BusArrive(Bus bus)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Bus arriving at bus stop to pick passengers");
+            while(bus.Space >0 && _personWaiting.Count > 0)
+            {
+                Passenger passenger =_personWaiting.Dequeue(); //return dequeued item
+                bus.Load(passenger);
+            }
+        }
+
+        public void PersonArrive(Passenger person)
+        {
+            _personWaiting.Enqueue(person);
+            Console.WriteLine($"{person} Queuing at Bus Stop");
+        }
+
+
+
+
+    }
+}
